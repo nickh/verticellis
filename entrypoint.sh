2abba8783c200c8233d525a3f081042fc1682224
Nick Hengeveld <nickh@github.com>
2020-05-31 15:04:32 -0700

    Split

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+VERTICELLIS=`pwd`
+
+echo "What's in here:"
+ls -alR
+echo -n "\n======\n"
 cd $GITHUB_WORKSPACE
+echo "What's in there:"
 ls -alR
 cd master
-ruby src/main.rb
+
+ruby ${VERTICELLIS}/src/main.rb
